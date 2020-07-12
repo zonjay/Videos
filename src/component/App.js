bcd933ee03aa72dba6e43fa4d840c4fdb24a7036
Jay <killua0530@gmail.com>
2020-07-12 07:24:09 +0000

    feat: create Header for App

---
+++ src/component/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../dist/app.scss';
+import Header from './Header';
+
+export default function App () {
+    return (
+        <Header />
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './component/App'
 
 function Index () {
-    return <div>test</div>
+    return <App />
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'));
